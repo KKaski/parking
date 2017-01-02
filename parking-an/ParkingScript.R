e3ea36e4064487148c9f86108cc66ab6dbbadfd0
KKaski <kimmo.kaskikallio@fi.ibm.com>
2017-01-02 21:16:28 +0200

    analytics part added

---
+++ parking-an/ParkingScript.R
@@ -0,0 +1,29 @@
+library(ibmdbR)
+#library(plotly)
+mycon <- idaConnect("BLUDB", "", "")
+idaInit(mycon)
+
+spaces <- as.data.frame(ida.data.frame('"DASH100925"."PLUGITCHARGERS"')[ ,c('DATE', 'DATEFULL', 'DAY', 'HOURS', 'MONTH', 'P10', 'P3', 'P5', '_ID', '_REV')])
+
+names(spaces)
+
+#LEts plot some histograms to see the data
+hist(as.numeric(spaces$P3))
+hist(as.numeric(spaces$P5))
+hist(as.numeric(spaces$P10))
+
+#Now some experimenting with the days
+plot(spaces$DAY,spaces$P3,main="Free spaces/day P3", xlab="Weekday", ylab="Free Spaces", pch=19)
+plot(spaces$DAY,spaces$P5,main="Free spaces/day P5", xlab="Weekday", ylab="Free Spaces", pch=19)
+plot(spaces$DAY,spaces$P10,main="Free spaces/day P10", xlab="Weekday", ylab="Free Spaces", pch=19)
+
+#P10 Seem interesting so lets get some zoom in by using hours
+plot(spaces$HOUR,spaces$P10,main="Free spaces/hour P10", xlab="Hour of day", ylab="Free Spaces P10", pch=19)
+
+#Lets filter to days 1 and 2
+spacesday1 <- spaces[spaces[, "DAY"]==1, ]
+plot(spacesday1$HOUR,spacesday1$P10,main="Free spaces Day1 /hour P10", xlab="Hour of day", ylab="Free Spaces P10", pch=19)
+
+spacesday2 <- spaces[spaces[, "DAY"]==2, ]
+plot(spacesday2$HOUR,spacesday2$P10,main="Free spaces Day2 /hour P10", xlab="Hour of day", ylab="Free Spaces P10", pch=19)
+
---
+++ parking-an/README.md
@@ -0,0 +1,14 @@
+#parking-an
+
+This is a light analytics piece for the parking application. The purpose of the content is to provide fast overview
+of some bluemix analytics capabilities you can use.
+
+##Setup and run
+1. Create and Bind dashDB to the parking-nr application
+2. Create warehouse link from cloudant to dashDB. This creates schema and start moving the data
+3. Open dashDB console and observe the tables created for you
+4. Load some data from earlier implementations by loading testdata.csv to your instance
+5. Go to the Analytic/ R Script
+6. Copy the content from the ParkingScript.r to the edit area and press sbmit
+7. Explore the script and the out put
+8. You can allso open RStudi environment where you can get nicer plots. If the environment asks for userids you can get them from the connect section of the dashDB console.
